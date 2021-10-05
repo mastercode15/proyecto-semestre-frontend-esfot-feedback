5138c03d09063f9e302e78523d4a311d9ea09718
mastercode15 <sebastian.morales@epn.edu.ec>
2021-10-05 00:31:49 -0500

    #42 se actualiza la página una vez que el estudiante envía una encuesta mostrando un mensaje de confirmación

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -1,71 +1,112 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout, Avatar, Image, Col, Row, Radio, Input, Button } from "antd";
 import ShowError from "../components/ShowError";
 import { useQuestionsList } from "../data/useQuestionsList";
 import QuestionsList from "../components/QuestionsList";
+import { CheckCircleOutlined, HomeOutlined } from '@ant-design/icons';
+import Routes from '../constants/routes';
+import { Link } from 'react-router-dom';
 
 const { Sider, Content } = Layout;
 const question = QuestionsList;
 
 const PrivatePage = (props) => {
-    console.log(props.location.data);
-    const data = props.location.data;
-    return <>
-        <Layout>
 
-            <Sider>
-                <div>
-                    <Avatar size={100} icon={<Image
-                        width={100}
-                        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />} />
-                </div>
-
-            </Sider>
-            <Content>
+    // console.log(props.location.data);
+    const [data, setData] = useState(undefined)
 
 
-                <div className={'container'} style={{ fontSize: 'x-large' }}>
-                    <Col align={'center'}>
-                        <h1>Encuesta</h1>
-                    </Col>
-                    <Row align={'center'}>
-                        <Col span={12} align={'center'}>
-                            <h3>Materia: {'espacio para la materia'}</h3>
-                            <h3>Docente: {'espacio para el docente'}</h3>
+    useEffect(() => {
+        if (localStorage["survey"] == undefined) {
+            console.log("no hay nada ")
+        } else {
+            setData(JSON.parse(localStorage["survey"]));
+            console.log("si hay algo")
+
+        }
+        console.log(data);
+
+    }, [window.localStorage])
+
+    if (data == undefined) {
+        return <>
+            <div style={{ textAlign: "center" }}>
+                <CheckCircleOutlined
+                    style={{ fontSize: "153px", color: "#1F3F6C" }} />
+                <br />
+                <h2>Encuesta enviada exitosamente</h2>
+                <br />
+                <h2>Te agradecemos</h2>
+                <h2>Tu opinión es importante para que exista un mejoramiento continuo</h2>
+                <Link to={Routes.HOME}>
+                    <Button type="primary" icon={<HomeOutlined />}>
+                        Inicio
+                    </Button>
+                </Link>
+
+            </div>
+        </>
+    } else {
+        return <>
+            <Layout>
+
+                <Sider>
+                    <div>
+                        <Avatar size={100} icon={<Image
+                            width={100}
+                            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />} />
+                    </div>
+
+                </Sider>
+
+                <Content>
+
+
+                    <div className={'container'} style={{ fontSize: 'x-large' }}>
+                        <Col align={'center'}>
+                            <h1>Encuesta</h1>
                         </Col>
-                        <Col span={12} align={'center'}>
-                            <h3>
-                                Tema: {data.Topic}
-                            </h3>
-                        </Col>
+                        <Row align={'center'}>
+                            <Col span={12} align={'center'}>
+                                <h3>Materia: {data.subject}</h3>
+                                <h3>Docente: {data.teacher}</h3>
+                            </Col>
+                            <Col span={12} align={'center'}>
+                                <h3>
+                                    Tema: {data.chapter.Topic}
+                                </h3>
+                            </Col>
 
-                    </Row>
-                </div>
+                        </Row>
+                    </div>
 
-                <div className={'container'} style={{ fontSize: 'large', margin: '0 5%' }}>
-                    <br />
-                    <h4>Intsrucciones:</h4>
-                    <h4> Seleccione una calificación para la pregunta siendo 1 la calificación mas baja y 5 la
-                        calificación mas alta</h4>
-                    <br />
-
-
-                    {
-                        useQuestionsList.isLoading
-                            ? 'cargando...'
-                            : question.isError
-                                ? <ShowError error={question.isError} />
-                                : <QuestionsList answers={data.answers} />
-                    }
-                    <br />
-                </div>
+                    <div className={'container'} style={{ fontSize: 'large', margin: '0 5%' }}>
+                        <br />
+                        <h4>Intsrucciones:</h4>
+                        <h4> Seleccione una calificación para la pregunta siendo 1 la calificación mas baja y 5 la
+                            calificación mas alta</h4>
+                        <br />
 
 
-            </Content>
-        </Layout>
+                        {
+                            useQuestionsList.isLoading
+                                ? 'cargando...'
+                                : question.isError
+                                    ? <ShowError error={question.isError} />
+                                    : <QuestionsList
+                                        // answers={data.answers} 
+                                        onChange={(value) => setData(value)} />
+                        }
+                        <br />
+                    </div>
 
 
-    </>;
+                </Content>
+            </Layout>
+
+
+        </>;
+    }
 
 
 }
