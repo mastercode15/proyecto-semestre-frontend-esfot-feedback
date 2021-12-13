4c2af3e5b68b69676a195cdd20437a402d768e30
mastercode15 <sebastian.morales@epn.edu.ec>
2021-12-12 21:05:19 -0500

    #55 mejorando la presentación de la página del logout

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,29 +1,36 @@
-import React, { useEffect } from 'react';
-import { useAuth } from '../providers/Auth';
-import withAuth from '../hocs/withAuth';
-import API from '../data';
-import Cookies from 'js-cookie';
+import React, { useEffect } from "react";
+import { useAuth } from "../providers/Auth";
+import withAuth from "../hocs/withAuth";
+import API from "../data";
+import Cookies from "js-cookie";
+import { Spin } from "antd";
 
 const Logout = () => {
   const { setAuthenticated, setCurrentUser } = useAuth();
-  useEffect( () => {
+  useEffect(() => {
     async function doLogout() {
       try {
-        console.log( 'loggin out' );
-        await API.post( '/logout' );
-        Cookies.remove( 'token' );
-        delete API.headers[ 'Authorization' ];
-        window.localStorage.setItem( 'login', JSON.stringify( false ) );
-        setAuthenticated( false );
-        setCurrentUser( null );
-      } catch( e ) {
-        console.log( 'e', e );
+        console.log("loggin out");
+        await API.post("/logout");
+        Cookies.remove("token");
+        delete API.headers["Authorization"];
+        window.localStorage.setItem("login", JSON.stringify(false));
+        setAuthenticated(false);
+        setCurrentUser(null);
+        window.location.reload(true);
+      } catch (e) {
+        console.log("e", e);
       }
     }
 
     doLogout();
-  }, [ setAuthenticated ] );
-  return <p>Logging out...</p>;
+  }, [setAuthenticated, setCurrentUser]);
+  return (
+    <div style={{ textAlign: "center", marginTop: "8%" }}>
+      <Spin size="large" />
+      <h2 style={{ marginTop: 50 }}>Cerrando Sesión</h2>
+    </div>
+  );
 };
 
-export default withAuth( Logout, '/' );
+export default withAuth(Logout, "/");
